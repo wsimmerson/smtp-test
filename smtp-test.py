54344661fb7572bf6fff99d0c6d9d43d39850788
Wayne Simmerson <wsimmerson@gmail.com>
2015-12-22 12:03:06 -0500

    added interactive mode, need to disable echo for password

--- smtp-test.py
+++ smtp-test.py
@@ -2,10 +2,12 @@
 
 import smtplib
 import argparse
+import sys
+
 
 def testmail(username, password, server, deliveryaddr):
     header = "From: %s\r\nTo: %s\r\nSubject: SMTP Test\r\n\r\nTest Successful" % (username, deliveryaddr)
-    
+
     try:
         server = smtplib.SMTP(server)
         server.login(username, password)
@@ -15,19 +17,38 @@ def testmail(username, password, server, deliveryaddr):
     else:
         print("Test Successful")
 
+
+def get_interactive_data():
+    class Data():
+        user = ''
+        password = ''
+        server = ''
+        dest = ''
+
+    data = Data()
+
+    data.user = input("Username for SMTP Login: ")
+    data.password = input("\nPassword for SMTP Login: ")
+    data.server = input("\nSMTP Server URL: ")
+    data.dest = input("\nDestination Email Address: ")
+
+    return data
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--user', required=True,
-            help="Username for SMTP Login")
+                        help="Username for SMTP Login")
     parser.add_argument('-p', '--password', required=True,
-            help="Password for SMTP Login")
+                        help="Password for SMTP Login")
     parser.add_argument('-s', '--server', required=True,
-            help="SMTP Server url")
+                        help="SMTP Server URL")
     parser.add_argument('-d', '--dest', required=True,
-            help='Destination Email Address')
+                        help='Destination Email Address')
 
-    args = parser.parse_args()
+    if len(sys.argv) == 2 and sys.argv[1].lower() == '-i':
+        args = get_interactive_data()
+    else:
+        args = parser.parse_args()
 
     testmail(args.user, args.password, args.server, args.dest)
-
